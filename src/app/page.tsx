a5c7a41e382cd91f15c70ad842d504b6383b925b
Michael Eisner <128039782+MJE43@users.noreply.github.com>
2024-07-02 09:00:46 +0000

    Added a Meta component with title and description for the homepage.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,16 @@ import CallToActionSection from '../components/CallToActionSection';
 import TestimonialsSection from '../components/TestimonialsSection';
 import RecentBlogPostsSection from '../components/RecentBlogPostsSection';
 import SocialMediaSection from '../components/SocialMediaSection';
+import Meta from '../components/Meta';
 
 function Home() {
   return (
     <>
-      <HeroSection />
+      <Meta 
+        title="Kyle Price Power and Electrical Services" 
+        description="Your Friendly Neighborhood Electrician in Tucson, Arizona"
+      />
+      <HeroSection phoneNumber="+15205551234" />
       <IntroductionSection />
       <FeaturedServicesSection />
       <CallToActionSection />
