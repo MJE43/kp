a7fa68c0febcf79cae02613b4f197ba0e0a42a71
Mike Eisner <eisner.michaelj@gmail.com>
2024-07-23 12:22:07 -0700

    Expanded the Tailwind content paths to include all files in the src directory.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,8 @@
-import type { Config } from 'tailwindcss'
+import { type Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
@@ -20,4 +18,4 @@ const config: Config = {
   plugins: [require("tailwindcss-animate")],
 }
 
-export default config
+export default config;
