dbada32e008df489262852b917902c8acd3a4784
Mike Eisner <eisner.michaelj@gmail.com>
2024-07-23 16:49:38 -0700

    updated typography to playfair noir and georgia

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,5 @@
 import type { Metadata } from 'next';
-import { Montserrat, Open_Sans } from 'next/font/google';
 import '../styles/globals.css';
-
-const montserrat = Montserrat({ 
-  subsets: ['latin'],
-  variable: '--font-montserrat',
-  display: 'swap',
-});
-
-const openSans = Open_Sans({ 
-  subsets: ['latin'],
-  variable: '--font-open-sans',
-  display: 'swap',
-});
 
 export const metadata: Metadata = {
   title: 'K.P. Power',
@@ -25,8 +12,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={`${montserrat.variable} ${openSans.variable}`}>
-      <body className="font-sans antialiased bg-gradient-to-br from-background-start to-background-end min-h-screen flex flex-col">
+    <html lang="en">
+      <body className="antialiased bg-gradient-to-br from-background-start to-background-end min-h-screen flex flex-col">
         <main className="flex-grow">
           {children}
         </main>
